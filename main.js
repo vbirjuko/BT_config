bbe25cdf4f770446fccb29d9b4571ac9bd9e880b
vbirjuko <89239270+vbirjuko@users.noreply.github.com>
2021-09-03 13:00:31 +0300

    Update main.js

--- main.js
+++ main.js
@@ -109,13 +109,20 @@ function connectDeviceAndCacheCharacteristic(device) {
         log('Service found, getting characteristic...');
 
         return service.getCharacteristic('14996d8a-8669-0ed5-2d5e-a892bd1d6212');
-        return service.getCharacteristic('74c1bfe8-2221-c915-c9fc-dd941f10f215');
       }).
       then(characteristic => {
         log('Characteristic found');
         characteristicCache = characteristic;
 
-        return characteristicCache;
+        return characteristicLeftCache;
+      });
+          return service.getCharacteristic('74c1bfe8-2221-c915-c9fc-dd941f10f215');
+      }).
+      then(characteristic => {
+        log('Characteristic found');
+        characteristicCache = characteristic;
+
+        return characteristicRightCache;
       });
 }
 
